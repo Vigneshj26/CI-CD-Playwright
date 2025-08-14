4bc91924be4394046e10d54a0e6bac3269e9183d
vignesh.jayavelu <vignesh.jayavelu@anubavam.com>
2025-08-14 15:22:31 +0530

    EnvFile

--- tests/login_env.spec.ts
+++ tests/login_env.spec.ts
@@ -1,21 +1,21 @@
-import test from "playwright/test";
-import dotenv from 'dotenv'
-import { env } from "process";
+// import test from "playwright/test";
+// import dotenv from 'dotenv'
+// import { env } from "process";
 
 
 
-dotenv.config({ path: `utils/Prod.env` })
-test('Testing the Env file', async ({ page }) => {
+// dotenv.config({ path: `utils/Prod.env` })
+// test('Testing the Env file', async ({ page }) => {
 
-    let user = process.env.LT_UserName as string
-    let pwd = process.env.LT_Password as string
+//     let user = process.env.LT_UserName as string
+//     let pwd = process.env.LT_Password as string
 
-    //console.log(process.env.LT_UserName)
+//     //console.log(process.env.LT_UserName)
 
-    await page.goto("https://qa.creatrixcampus.net/auth/login")
-    await page.getByPlaceholder("Username").fill(user)
-    await page.getByPlaceholder("Password").fill(pwd)
-    await page.locator("#kt_login_signin_submit").click()
+//     await page.goto("https://qa.creatrixcampus.net/auth/login")
+//     await page.getByPlaceholder("Username").fill(user)
+//     await page.getByPlaceholder("Password").fill(pwd)
+//     await page.locator("#kt_login_signin_submit").click()
 
 
-})
+// })
