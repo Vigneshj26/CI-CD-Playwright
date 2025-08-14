ae0f6a445d8e40b67c5d81aa049b44dcbe0ec4e5
vignesh.jayavelu <vignesh.jayavelu@anubavam.com>
2025-08-14 15:17:04 +0530

    DataParametraization

--- tests/DataParametarization.spec.ts
+++ tests/DataParametarization.spec.ts
@@ -1,19 +1,19 @@
-import test from '@playwright/test'
-import data from "../utils/Login.json"
+// import test from '@playwright/test'
+// import data from "../utils/Login.json"
 
-for (let value of data) {
+// for (let value of data) {
 
-    test(`Test Login functionality ${value['TestCase-Id']}`, async ({ page }) => {
-
-
-        //console.log(" Username for the application", value.UserName)
-
-        await page.goto("https://qa.creatrixcampus.net/auth/login")
-        await page.getByPlaceholder("Username").fill(value.UserName)
-        await page.getByPlaceholder("Password").fill(value.Password)
-        await page.locator("#kt_login_signin_submit").click()
+//     test(`Test Login functionality ${value['TestCase-Id']}`, async ({ page }) => {
 
 
+//         //console.log(" Username for the application", value.UserName)
 
-    });
-}
+//         await page.goto("https://qa.creatrixcampus.net/auth/login")
+//         await page.getByPlaceholder("Username").fill(value.UserName)
+//         await page.getByPlaceholder("Password").fill(value.Password)
+//         await page.locator("#kt_login_signin_submit").click()
+
+
+
+//     });
+// }
